8b5b0b599638b38367afa8c435314e1927ee7bbb
Nicholas Landau <nrlandau@gmail.com>
2018-11-28 13:17:01 -0500

    Added GetAllCars and GetCar to the API

--- CarDealerAPI/Controllers/CarController.cs
+++ CarDealerAPI/Controllers/CarController.cs
@@ -10,19 +10,52 @@ namespace CarDealerAPI.Controllers
 {
     public class CarController : ApiController
     {
+        Models.GCCarDealerShipEntities cDB = new Models.GCCarDealerShipEntities();
         [HttpGet]
         public JObject GetAllCars()
         {
-            JObject jObject = new JObject();
-            jObject["Hello"] = "Test";
-            jObject["Test"] = "Hello";
+            List<Models.Car> cars = cDB.Cars.ToList();
+            JObject jCars = new JObject();
+            jCars["Count"] = cars.Count;
             JArray temp = new JArray();
-            jObject["Hmm"] = new JArray();
+            foreach(Models.Car car in cars)
+            {
+                JObject jCar = new JObject();
+                jCar["Id"] = car.Id;
+                jCar["Make"] = car.Make;
+                jCar["Model"] = car.Model;
+                jCar["Year"] = car.year;
+                jCar["Color"] = car.color;
+                temp.Add(jCar);
+            }
+            jCars["Cars"] = temp;
+
+            //JObject jObject = new JObject();
+            //jObject["Hello"] = "Test";
+            //jObject["Test"] = "Hello";
+            //JArray temp = new JArray();
+            //jObject["Hmm"] = new JArray();
 
             
             //jObject["Hmm"][0] = "Test";
 
-            return jObject;
+            return jCars;
+        }
+        [HttpGet]
+        public JObject GetCar(int Id)
+        {
+            Models.Car car = cDB.Cars.Find(Id);
+            JObject jCar = new JObject();
+            if (car == null)
+            {
+                return jCar;
+            }
+            jCar["Id"] = car.Id;
+            jCar["Make"] = car.Make;
+            jCar["Model"] = car.Model;
+            jCar["Year"] = car.year;
+            jCar["Color"] = car.color;
+            return jCar;
         }
     }
 }
