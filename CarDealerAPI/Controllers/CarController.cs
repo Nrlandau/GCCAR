67e82ae7cc4efdc9d93f603726f224c877e416ef
Nicholas Landau <nrlandau@gmail.com>
2018-11-28 13:46:38 -0500

    Put ToJSON into own function and added GetAllCars with params

--- CarDealerAPI/Controllers/CarController.cs
+++ CarDealerAPI/Controllers/CarController.cs
@@ -14,37 +14,28 @@ namespace CarDealerAPI.Controllers
         [HttpGet]
         public JObject GetAllCars()
         {
-            List<Models.Car> cars = cDB.Cars.ToList();
-            JObject jCars = new JObject();
-            jCars["Count"] = cars.Count;
-            JArray temp = new JArray();
-            foreach(Models.Car car in cars)
-            {
-                JObject jCar = new JObject();
-                jCar["Id"] = car.Id;
-                jCar["Make"] = car.Make;
-                jCar["Model"] = car.Model;
-                jCar["Year"] = car.year;
-                jCar["Color"] = car.color;
-                temp.Add(jCar);
-            }
-            jCars["Cars"] = temp;
-
-            //JObject jObject = new JObject();
-            //jObject["Hello"] = "Test";
-            //jObject["Test"] = "Hello";
-            //JArray temp = new JArray();
-            //jObject["Hmm"] = new JArray();
-
+            return CarsToJson(cDB.Cars.ToList());
             
-            //jObject["Hmm"][0] = "Test";
-
-            return jCars;
         }
         [HttpGet]
         public JObject GetCar(int Id)
         {
             Models.Car car = cDB.Cars.Find(Id);
+            
+            return CarToJson(car);
+        }
+        [HttpGet]
+        public JObject GetAllCars(string make, string model, int? year, string color)
+        {
+            return CarsToJson(cDB.Cars.Where(x => (make != null ? x.Make == make : true) && 
+                                            (model != null ? x.Model == model : true) && 
+                                            (year != null ? x.year == year : true) &&
+                                            (color != null ? x.color == color : true))
+                                            .ToList());
+        }
+        //How One Car is in JSON
+        private JObject CarToJson(Models.Car car)
+        {
             JObject jCar = new JObject();
             if (car == null)
             {
@@ -56,6 +47,19 @@ namespace CarDealerAPI.Controllers
             jCar["Year"] = car.year;
             jCar["Color"] = car.color;
             return jCar;
+        }
+        //How Multiple Cars are in JSON
+        private JObject CarsToJson(IEnumerable<Models.Car> cars)
+        {
+            JObject jCars = new JObject();
+            jCars["Count"] = cars.Count(); JArray temp = new JArray();
+            foreach (Models.Car car in cars)
+            {
+                temp.Add(CarToJson(car));
+            }
+            jCars["Cars"] = temp;
+            return jCars;
+
         }
     }
 }
