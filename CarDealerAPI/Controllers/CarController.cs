f89524cb6aa079015d8a0236f7f18c1ea6b208cd
Nicholas Landau <nrlandau@gmail.com>
2018-11-28 13:02:53 -0500

    Added DB to API

---
+++ CarDealerAPI/Controllers/CarController.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CarDealerAPI.Controllers
+{
+    public class CarController : ApiController
+    {
+        [HttpGet]
+        public JObject GetAllCars()
+        {
+            JObject jObject = new JObject();
+            jObject["Hello"] = "Test";
+            jObject["Test"] = "Hello";
+            JArray temp = new JArray();
+            jObject["Hmm"] = new JArray();
+
+            
+            //jObject["Hmm"][0] = "Test";
+
+            return jObject;
+        }
+    }
+}
